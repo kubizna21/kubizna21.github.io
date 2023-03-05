5d10ca5ec3e0b67c97aa064a2dc6103e4a8c851f
Ashley Kubizna <113566558+kubizna21@users.noreply.github.com>
2023-03-05 11:26:50 -0800

    added to my paddle.js

---
+++ paddle.js
@@ -0,0 +1,33 @@
+const SPEED = 0.02
+/*computers maximum speed for the ball */
+
+export default class Paddle {
+  constructor(paddleElem) {
+    this.paddleElem = paddleElem
+    this.reset()
+  }
+
+  get position() {
+    return parseFloat(
+      getComputedStyle(this.paddleElem).getPropertyValue("--position")
+    )
+  }
+
+  set position(value) {
+    this.paddleElem.style.setProperty("--position", value)
+  }
+
+  rect() {
+    return this.paddleElem.getBoundingClientRect()
+  }
+
+  reset() {
+    this.position = 50
+  }
+
+  update(delta, ballHeight) {
+    this.position += SPEED * delta * (ballHeight - this.position)
+    /*if the ball is above current position it would go upward and if its below current position it would move downward
+    when ball moves too fast the computer will no longer be able to keep up with it */
+  }
+}
